0401fd80258a9e25d5f29404e663fdcf80827b69
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 20:30:45 +0530

    feat(service): add FileService interface for file operations
    - Define methods to create, delete, and download resources.
    - Uses CommonFileSavedBinaryDataDTO for storing file metadata.
    - Supports MultipartFile uploads and bucket-based file management.

---
+++ src/main/java/com/travel_ease/hotel_system/service/FileService.java
@@ -0,0 +1,10 @@
+package com.travel_ease.hotel_system.service;
+
+import com.travel_ease.hotel_system.util.CommonFileSavedBinaryDataDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+    public CommonFileSavedBinaryDataDTO createResource(MultipartFile file, String directory, String bucket);
+    public void deleteResource(String bucket,String directory, String fileName);
+    public byte[] downloadFile(String bucket, String fileName);
+}
