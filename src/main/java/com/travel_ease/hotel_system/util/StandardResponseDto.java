e4164e3f39b368c400a47c4266f39a0653e403c5
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 20:19:01 +0530

    feat: add StandardResponseDto for consistent API responses

---
+++ src/main/java/com/travel_ease/hotel_system/util/StandardResponseDto.java
@@ -0,0 +1,14 @@
+package com.travel_ease.hotel_system.util;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StandardResponseDto {
+    private int code;
+    private String message;
+    private Object data;
+}
