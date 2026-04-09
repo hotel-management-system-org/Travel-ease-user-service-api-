2b9596a065a2fd26d82b0c3c2ce06dd592308177
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 20:13:30 +0530

    feat: add KeycloakException for handling Keycloak-related errors

---
+++ src/main/java/com/travel_ease/hotel_system/exception/KeycloakException.java
@@ -0,0 +1,7 @@
+package com.travel_ease.hotel_system.exception;
+
+public class KeycloakException extends RuntimeException {
+    public KeycloakException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
