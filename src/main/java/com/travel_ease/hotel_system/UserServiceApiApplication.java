86eba5589d0e1ca14dc6b97e74238305da4955ac
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-16 12:41:08 +0530

    refactor: remove @EnableDiscoveryClient from user service

--- src/main/java/com/travel_ease/hotel_system/UserServiceApiApplication.java
+++ src/main/java/com/travel_ease/hotel_system/UserServiceApiApplication.java
@@ -2,10 +2,8 @@ package com.travel_ease.hotel_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserServiceApiApplication {
 
 	public static void main(String[] args) {
